58649005dac68fccc47568cd31d5391a798e4fb8
Wesley Peak <wesleypeak@macbook-pro.lan>
2024-03-12 23:28:00 -0500

    About added and formatted

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             </div>
         </div>
 
-        <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
         >
@@ -40,7 +40,7 @@ export default function Intro() {
             <span className="font-bold">6+ years</span> of experience. I enjoy
             working with people. My current focus is to become a {" "} <span className="font-bold">Cloud Engineer
             </span>. I'm also training in {" "}<span className="underline">React (Next.js)</span>.
-        </motion.p>
+        </motion.h1>
 
         <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium'
             initial={{ opacity: 0, y: 100 }}
