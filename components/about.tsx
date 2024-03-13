610fff94bd6fda85a39e6dd06a6d3d70a861b257
Wesley Peak <wesleypeak@macbook-pro.lan>
2024-03-12 23:30:18 -0500

    About added and formatted

---
+++ components/about.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function About() {
+  return <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+    <h2 className='text-3xl font-medium mb-8'>about me</h2>
+    <p className="mb-3">
+        A long way off from a computer science degree, I originally graduated with a degree in{" "}
+        <span className="font-medium">English</span>. This degree has given me a great foundation in the ability to comunicate. 
+        After 3 years of teaching, I decided to pursue a new career in programming. I am a proud graduate of the{" "}
+        <span className="font-medium">Launchcode</span> program through <span className="font-medium">UMKC</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is to see design come to life. 
+        I deeply <span className="underline">enjoy</span> seeing a project unfold like a painting. Due to my communication skills 
+        and my love of helping others, I have excelled in being a{" "} 
+        <span className="font-medium">Technical Support Engineer</span>. This had led me on a great journey, working for some 
+        great companies and has provided me with a robust tool set for taking on technical challenges. I am currently looking for 
+        a{" "} <span className="font-medium">full-time position</span> as a{" "}<span className="font-medium">Senior Support Engineer</span>. 
+      </p>
+
+      <p>
+        <span className="italic">In my downtime</span>, I enjoy spending time with family and friends, making music, and reading. 
+        I'll also bust out some DJ skills if the right person asks. When I am not engaged in those activities, I enjoy{" "} 
+        <span className="font-medium">learning new things</span>. I am currently studying to become a{" "}
+        <span className="font-medium">Cloud Engineer</span>. 
+      </p>
+  </section>
+  
+}
