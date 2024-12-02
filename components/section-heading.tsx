b39aab0599bd16d3677f4fc4bebff16d3268bb80
Wesley Peak <wesley.peak@gmail.com>
2024-12-01 23:38:17 -0600

    6th attempt

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className="text-4xl font-medium mb-8 text-center flex flex-wrap">
+    <h2 className="text-4xl font-medium mb-8 text-center flex-wrap">
       {children}
     </h2>
   );
