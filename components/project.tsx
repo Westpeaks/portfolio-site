26559b6a827c8562b14e70362125dafba0c9d364
Wesley Peak <wesley.peak@gmail.com>
2024-12-01 23:11:59 -0600

    FIxed image overlap in project cards.

--- components/project.tsx
+++ components/project.tsx
@@ -25,8 +25,8 @@ export default function Project( {title, description, tags, hoverUrl, imageUrl}:
         <section className='bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 
         relative sm:h-[20rem] hover:bg-gray-200 transition group-even:pl-8'>
 
-            <div className='pt-4 pb-7 px-5 sm:pl-10 pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full 
-            group-even:ml-[18rem]'>
+            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full 
+            group-even:ml-[18rem] group-odd:mr-[18rem]'>
             <h3 className='text=2xl font-semibold'>{title}</h3>
             <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
             <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -37,9 +37,9 @@ export default function Project( {title, description, tags, hoverUrl, imageUrl}:
             </ul>
             </div>
             <a href={hoverUrl} target='blank'>
-                <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute top-8 -right-40 
-                w-[28.25rem] rounded-t-lg shadow-2xl transition 
-
+                <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute sm:block top-8 -right-40 
+                w-[28.25rem] rounded-t-lg shadow-2xl 
+                transition 
                 group-hover:scale-[1.04] 
                 goup-hover:-translate-x-3 
                 group-hover:translate-y-3 
@@ -49,7 +49,7 @@ export default function Project( {title, description, tags, hoverUrl, imageUrl}:
                 group-even:group-hover:translate-y-3 
                 group-even:group-hover:rotate-2  
                 group-even:right-[initial] 
-                group-even:-left-40' />           
+                group-even:-left-40'/>           
             </a>
         </section>
     </motion.div>
