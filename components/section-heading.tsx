8f273ac5fbf2fd61dfcd07e2bae3cc4f0eedd6cb
Wesley Peak <wesley.peak@gmail.com>
2024-12-02 11:48:53 -0600

    remove hyphens

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className='text-4xl font-medium mb-8 text-center break-words hyphens-auto'> 
+    <h2 className='text-4xl font-medium mb-8 text-center break-words'> 
         {children}
     </h2>
   );
