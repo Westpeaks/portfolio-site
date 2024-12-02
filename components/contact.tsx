a9f739bf0b19c2e45029665ca9a255a69383ab10
Wesley Peak <wesley.peak@gmail.com>
2024-12-01 23:20:01 -0600

    Fixed contact text to wrap.

--- components/contact.tsx
+++ components/contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
 
     return (
         <section id='contact' className='mb-20 sm:mb-28 text-lg'>
-            <p className='flex items-center justify-center'> contact me on <a className='flex items-center 
+            <p className='flex flex-wrap items-center justify-center'> contact me on <a className='flex items-center 
             justify-center ml-1' href='https://www.linkedin.com/in/wesley-peak/' target='blank'>Linked
                 <span className='ml-0.5 mr-1'><BsLinkedin /></span></a> or at
                 <a className=' underline ml-1' 
