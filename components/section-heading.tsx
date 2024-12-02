d94efccb453a295d74aa1ba913142e6d58b490b5
Wesley Peak <wesley.peak@gmail.com>
2024-12-02 11:06:34 -0600

    Trying to fix title overlap on mobile in sectionheader.

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className="text-4xl font-medium mb-8 text-center">
+    <h2 className="text-4xl sm:text-2xl font-medium mb-8 text-center">
       <span className='inline-block break-words hyphens-auto'> 
         {children}
       </span>
